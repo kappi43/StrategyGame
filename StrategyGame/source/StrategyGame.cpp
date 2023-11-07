5347a8e77214cea7ae4f8b6d9173fa48542f1955
kappi43 <kappi43@gmail.com>
2023-11-07 18:45:39 +0100

    Starting the repo
    Adding a working hello world

---
+++ StrategyGame/source/StrategyGame.cpp
@@ -0,0 +1,12 @@
+﻿// StrategyGame.cpp : Defines the entry point for the application.
+//
+
+#include "StrategyGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
